0a50a7e04ff10d61a10cc6550871f4949441b2fd
Gus Rodríguez <43864523+gusrodzu@users.noreply.github.com>
2023-05-12 07:49:30 -0600

    Actualizado Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,51 +12,59 @@ const Form = (props) => {
         setErrors(validation({ ...userData, [e.target.name]: e.target.value }));
     };
 
-const handleSubmit = (e) => {
-    e.preventDefault ();
-    login(userData);
-}
-
+    const handleSubmit = (e) => {
+        e.preventDefault ();
+        login(userData);
+    }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Usuario</label>
-                <input
-                    type="text"
-                    placeholder='Email'
-                    name="email"
-                    onChange={handleChange}
-                    value={userData.email}
-                />
-                { errors.e1 ? (
-                    <p>{errors.e1}</p>
-                ) : errors.e2 ? (
-                    <p>{errors.e2}</p>
-                ) : (
-                    <p>{errors.e3}</p>
-                )}
+        <div className={styles["main-content"]}>
+            <div className={styles["company__info"]}>
+                <h2 className="row">LOGO</h2>
+                <h3 className="row">¡Bienvenido de nuevo!</h3>
+                
+            </div>
+            <div className={["form"]}>
+                <form onSubmit={handleSubmit}>
+                    <h2>Inicia sesión</h2>
+                    <label htmlFor="">Usuario</label>
+                    <input
+                        type="text"
+                        placeholder='Email'
+                        name="email"
+                        onChange={handleChange}
+                        value={userData.email}
+                        className={styles["form__input"]}
+                    />
+                    { errors.e1 ? (
+                        <p>{errors.e1}</p>
+                    ) : errors.e2 ? (
+                        <p>{errors.e2}</p>
+                    ) : (
+                        <p>{errors.e3}</p>
+                    )}
 
-                <label htmlFor="">Contraseña</label>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    onChange={handleChange}
-                    value={userData.password}
-                />
-                { errors.p1 ? (
-                    <p>{errors.p1}</p>
-                ) : (
-                    <p>{errors.p2}</p>
-                )}
+                    <label htmlFor="">Contraseña</label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleChange}
+                        value={userData.password}
+                        className={styles["form__input"]}
+                    />
+                    { errors.p1 ? (
+                        <p>{errors.p1}</p>
+                    ) : (
+                        <p>{errors.p2}</p>
+                    )}
 
-                <button>LOGIN</button>
-            </form>
-
-            <p>Datos de acceso Demo</p>
-            <p>Email: ejemplo@gmail.com</p>
-            <p>Contraseña: password123</p>
+                    <button className={styles["btn"]}>LOGIN</button>
+                </form>
+                <p>Datos de acceso Demo</p>
+                <p>Email: ejemplo@gmail.com</p>
+                <p>Contraseña = password123</p>
+            </div>
         </div>
     );
 };
